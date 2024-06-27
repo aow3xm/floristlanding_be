29b12bc88c6ecda274ef1a5d9e6e2c3a7dccffaa
aow3xm <149279753+aow3xm@users.noreply.github.com>
2024-06-27 23:37:41 +0700

    style(app) re-styling code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from './core/config';
 import { AuthModule } from './app/auth';
 import { UsersModule } from './app/users';
-import { EmailModule } from './core/email/email.module';
-import { RolesModule } from './app/roles/roles.module';
+import { EmailModule } from './core/email';
+import { ProductsModule } from './app/products/products.module';
+import { CollectionsModule } from './app/collections/collections.module';
 
 @Module({
   imports: [
@@ -12,8 +13,9 @@ import { RolesModule } from './app/roles/roles.module';
     ConfigModule,
     UsersModule,
     EmailModule,
-    RolesModule,
     forwardRef(() => AuthModule),
+    ProductsModule,
+    CollectionsModule,
   ],
   providers: [AppService],
 })
--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -2,3 +2,5 @@ export * from './auth.module';
 export * from './auth.service';
 export * from './jwt.guard';
 export * from './jwt.strategty';
+export * from './roles.decorator';
+export * from './roles.guard';
